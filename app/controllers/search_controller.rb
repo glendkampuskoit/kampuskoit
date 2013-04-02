22fc869f19727a44cc602fdb95e75685fc33fb93
Glend Maatita <glend.maatita@kampus.co.id>
2013-04-02 12:43:03 +0700

    remove thinking sphinx test

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,5 @@
 class SearchController < ApplicationController
   def result
+  	
   end
 end
--- spec/models/univ_spec.rb
+++ spec/models/univ_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
-require 'support/sphinx'
-
 describe "Searching for Articles" do
-  ThinkingSphinx::Test.run do
-    get :index
-    assert [@article], assigns[:articles]
+
   end
 end
