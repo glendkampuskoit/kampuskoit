b246bfe4d974abcabd1bacc1c2792e96d2e60dd5
Glend Maatita <glend.maatita@gmail.com>
2013-07-05 16:01:56 +0700

    add initial css & js for new front template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
   def index
   	subscriber = current_subscriber
 	if subscriber
-	  	survey = SurveyAlumni.find_by_subscriber_id(subscriber.id) if subscriber
-  		flash[:survey] = true if !survey 
+    survey = SurveyAlumni.find_by_subscriber_id(subscriber.id) if subscriber
+    flash[:survey] = true if !survey 
 	end
+
+  render :layout => false
 
   end
   
--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Campus
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
