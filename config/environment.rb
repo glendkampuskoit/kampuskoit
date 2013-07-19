f99ab78ff36eb1a19fadbc689a9f14d8aca71dd8
Glend Maatita <the_entung@yahoo.com>
2013-07-19 15:20:27 +0700

    reduce validator, show only 1 error message particularly

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require 'chronic'
+require 'reduce_validator'
 
 # Initialize the rails application
 Campus::Application.initialize!
