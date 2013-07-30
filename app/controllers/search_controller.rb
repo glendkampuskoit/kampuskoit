9fcf2bcb993b712a18429290c08e51db5895f14b
Glend Maatita <the_entung@yahoo.com>
2013-07-30 07:34:04 +0700

    fixing pagination on search all

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -11,12 +11,12 @@ class SearchController < ApplicationController
   	if params[:att] == 'prodi'
   		@provinsis = Provinsi.all
     	@jenjang_prodis = JenjangProdi.all
-  		@prodis = Prodi.search params[:keyword], :page => params[:prodi_page], :per_page => 10 
+  		@prodis = Prodi.search params[:keyword], :page => params[:page], :per_page => 10 
   		render :template => 'search/prodi_result'
   	else params[:att] == 'univ'
   		@provinsis = Provinsi.order("provinsi ASC")
     	@jenis_pts = JenisPt.order("jenis ASC")
-  		@univs = Univ.search params[:keyword], :page => params[:pt_page], :per_page => 10 
+  		@univs = Univ.search params[:keyword], :page => params[:page], :per_page => 10 
   		render :template => 'search/univ_result'
   	end
   end
