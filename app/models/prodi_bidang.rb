46525b929817789e7fa379863f3f954dfc94f4bc
Glend Maatita <the_entung@yahoo.com>
2013-07-31 15:56:55 +0700

    add lapangan kerja to prodi bidang

--- app/models/prodi_bidang.rb
+++ app/models/prodi_bidang.rb
@@ -3,6 +3,7 @@ class ProdiBidang < ActiveRecord::Base
 	validates :nama_bidang, presence: true, uniqueness: { case_sensitive: false }
 	validates :level, presence: true
 	validates :deskripsi, presence: true
+	validates :lapangan_kerja, presence: true
 
 	has_many :prodis
 end
