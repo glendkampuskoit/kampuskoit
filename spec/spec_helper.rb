ac24cb5af591b139e726b6d781faecec4f25c4e2
Glend Maatita <the_entung@yahoo.com>
2013-08-19 13:10:16 +0700

    add coverage test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'spork'
+require 'simplecov'
+
+# start simplecov
+SimpleCov.start 'rails'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
