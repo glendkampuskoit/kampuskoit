ed35c6d87ab7f58e56335d1d2303d8c90eff060e
agung <agung.laksono@hotmail.com>
2013-09-12 08:50:00 +0000

    merge to pull

--- spec/models/range_pekerjaan_pertama_spec.rb
+++ spec/models/range_pekerjaan_pertama_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe RangePekerjaanPertama do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @rangepekerjaanpertama = RangePekerjaanPertama.new(pekerjaan_pertama: "Staf IT", bobot: "1") }
+
+  subject{ @rangepekerjaanpertama }
+
+  it { should respond_to(:pekerjaan_pertama) }
+
+  describe "should valid" do
+  	it { should be_valid }
+  end
+
+  describe "When range is blank" do
+    before { @rangepekerjaanpertama.pekerjaan_pertama = "" }
+    it { should_not be_valid }
+  end
+
+  describe "range must be unique" do
+    before do
+      @rangepekerjaanpertama_dup = @rangepekerjaanpertama.dup
+      @rangepekerjaanpertama_dup.save
+    end
+    it { should_not be_valid }
+  end
 end
