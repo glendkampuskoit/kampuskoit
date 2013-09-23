e3d7bb880fdc0ee8a5c24f2a7ca5f3e63b85ea58
glendmaatita <glend.maatita@gmail.com>
2013-09-23 09:58:03 +0700

    fixing pagination

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -5,15 +5,15 @@ class RankingController < ApplicationController
 
 		#@univrankings = Univranking.filter_by_params(params).paginate(:page => params[:page], :per_page => 20)
 		#@univrankings = Univranking.where(:status => "1").order("score DESC").paginate(:page => params[:page], :per_page => 20)
-		@univrankings = Univranking.where(:status => "1").order("score DESC")
+		@univrankings = Univranking.where(:status => "1").order("score DESC").paginate(:page => params[:page], :per_page => 10) 
 		@title = "Peringkat Perguruan Tinggi"
 
 		if params[:provinsi]
 			@univs = Univ.joins(:kota).where("kotas.provinsi_id" => params[:provinsi])
-			@univrankings = Univranking.where(["univ_id IN (?)", @univs]).order("score DESC")
+			@univrankings = Univranking.where(["univ_id IN (?)", @univs]).order("score DESC").paginate(:page => params[:page], :per_page => 10) 
 			@title = "Peringkat Perguruan Tinggi - Provinsi #{Provinsi.find(params[:provinsi]).provinsi.titleize}"
 		elsif params[:kategori]
-			@univrankings = Univranking.joins(:univ).where("univs.jenis_pt_id" => params[:kategori] ).order("score DESC")
+			@univrankings = Univranking.joins(:univ).where("univs.jenis_pt_id" => params[:kategori] ).order("score DESC").paginate(:page => params[:page], :per_page => 10) 
 			@title = "Peringkat Perguruan Tinggi - Kategori #{JenisPt.find(params[:kategori]).jenis.titleize}"
 		end
 	end
