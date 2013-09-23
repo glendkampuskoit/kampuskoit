18d41dc3e9d18084debd7b9c66facadcba099376
glendmaatita <glend.maatita@gmail.com>
2013-09-23 10:41:33 +0700

    fixing error when create failed

--- app/controllers/survey_respondens_controller.rb
+++ app/controllers/survey_respondens_controller.rb
@@ -20,11 +20,21 @@ class SurveyRespondensController < ApplicationController
 
 	def create
 		@survey_responden = SurveyResponden.new(params[:survey_responden])
+		@range_gajis = RangeGaji.all
+		@range_bidang_kerjas = RangeBidangKerja.all
+		@range_waktu_tunggus = RangeWaktuTunggu.all
+		@range_lama_kuliahs = RangeLamaKuliah.all
+		@range_ipks	= RangeIpk.all
+		@range_pekerjaan_pertamas = RangePekerjaanPertama.all
+		@range_profesis = RangeProfesi.all
+		@range_pendidikan_terakhirs = RangePendidikanTerakhir.all
+		@range_gaji_pertamas = RangeGajiPertama.all
+		@range_level_karirs = RangeLevelKarir.all
 
 		if @survey_responden.save
 			redirect_to posts_path, :notice => "Terimakasih telah mengisi survey"
 		else
-			render "new"
+			render action: "new"
 		end
 	end
 end
