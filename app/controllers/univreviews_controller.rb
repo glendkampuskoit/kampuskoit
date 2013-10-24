a94844d011c98b31011933b16307c1dc6e6b3d15
glendmaatita <glend.maatita@gmail.com>
2013-10-24 13:58:08 +0700

    add univ review

--- app/controllers/univreviews_controller.rb
+++ app/controllers/univreviews_controller.rb
@@ -1,6 +1,15 @@
 class UnivreviewsController < ApplicationController
-
 	def new
-		@univreview = Univreview.new		
+		@univreview = Univreview.new
+		@points = %w( 1 2 3 4 5 6 7 8 9 10 )
+	end
+
+	def create
+		@univreview = Univreview.new(params[:univreview])
+		if @univreview.save
+			redirect_to univreviews_controller, :notice => "Terima Kasih telah mengisi survey. Silakan mengisi review dibawah ini"
+		else
+			render action: "new"
+		end
 	end
 end
