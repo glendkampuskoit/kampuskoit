fc721a9d7d1788f366f21726f8ae2636414ad8d0
glendmaatita <glend.maatita@gmail.com>
2013-10-25 14:03:03 +0700

    fixing rspec univreview errors

--- app/models/univreview.rb
+++ app/models/univreview.rb
@@ -1,14 +1,13 @@
 class Univreview < ActiveRecord::Base
 
-	belongs_to :univ
-	belongs_to :subscriber
+	belongs_to :univ, :foreign_key => :univ_id
+	belongs_to :subscriber, :foreign_key => :subscriber_id
 
 	validates :review, presence: true
-	validates :fasilitas_point, presence: true
-	validates :beasiswa_point, presence: true
-	validates :relevansi_point, presence: true
-
 	validates :univ, presence: true
 	validates :subscriber, presence: true
-
+	validates :beasiswa_point, presence: true
+	validates :relevansi_point, presence: true
+	validates :fasilitas_point, presence: true
+	
 end
