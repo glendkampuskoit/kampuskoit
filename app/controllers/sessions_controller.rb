54f6e42ce148c79b341f5265334c551dcd3ef089
glendmaatita <glend.maatita@gmail.com>
2013-10-25 13:11:51 +0700

    add review and comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
 	def new
+		if params[:univ_id]
+			@univ_id = params[:univ_id]
+		end
+
 		if current_subscriber
       redirect_to home_path
     else
@@ -11,6 +15,11 @@ class SessionsController < ApplicationController
 		subscriber = Subscriber.from_omniauth(env['omniauth.auth'])
 		sign_in(subscriber, 1) #login via facebook automatically add remember me
 		flash[:success] = 'Login sukses! Selamat datang di Kampus.co.id'
+
+		if params[:univ_id]
+			redirect_to new_survey_responden_path(:univ_id => @univ_id)
+		end
+		
 		redirect_to new_survey_responden_path
 	end
 
