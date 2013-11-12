90a5d435f1dc28972abc4ace41a675ac6fb693cf
glendmaatita <glend.maatita@gmail.com>
2013-11-12 14:32:59 +0700

    adding new files template

--- app/assets/javascripts/admin/ace-editor/sample/prototype.js
+++
@@ -1,6 +0,0 @@
-function myFunction()
-{
-    var obj=document.getElementById("h01");
-    obj.innerHTML="Hello Prototype";
-}
-onload=myFunction;
--- app/assets/javascripts/admin/ace-editor/sample/validate.js
+++
@@ -1,9 +0,0 @@
-function validateForm()
-{
-    var x=document.forms["myForm"]["fname"].value;
-    if (x==null || x=="")
-    {
-        alert("First name must be filled out");
-        return false;
-    }
-}
